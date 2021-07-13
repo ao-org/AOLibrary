546851840583e64f8a4d86881626d50e8a2c9f66
Jopi <jopiodz00@gmail.com>
2021-07-13 15:10:15 -0300

    Revert "feat: hasheo y validación de contraseñas BCrypt (#1)"
    This reverts commit 5a7c9891ddbabacd9e5827b27f3853ed5b6d32ff.

--- src/include/declares.h
+++ src/include/declares.h
@@ -6,9 +6,8 @@
 #define WIN32_LEAN_AND_MEAN // Excluir material rara vez utilizado de encabezados de Windows
 #define VC_EXTRALEAN        // Excluir material rara vez utilizado de encabezados de Windows (de MSVC)
 
-#ifdef _MSC_VER
-    // Creamos un alias con el nombre de la funcion sin decorar
-    #define UNDECORATE comment(linker, "/EXPORT:" __FUNCTION__ "=" __FUNCDNAME__)
-#endif
-
 #define EXPORT __declspec(dllexport)
+
+extern "C" {
+    void EXPORT CALLBACK CallHandle(DWORD address, int16_t userindex);
+}
